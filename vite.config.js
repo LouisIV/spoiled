95f182a1044804e36750eb6ce9b08883ffe9fe21
Louis Lombardo IV <louis@lombardo.work>
2024-09-02 10:33:44 -0700

    feat: 🎸 added webpage for casting

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,15 @@
-export default {
-  // Needed for GitHub Pages
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
   base: "/spoiled/",
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "cast.html"),
+      },
+    },
+  },
+});
